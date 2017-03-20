ca62335aee7b0c90f2427545e77892e1cddbad9b
DarthWolf13 <martijn.vander.meer@quicknet.nl>
2017-03-20 10:07:53 +0100

    Step 3: End of Class: Fire()

--- Centipede/Centipede/Bullet.cs
+++ Centipede/Centipede/Bullet.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,8 @@ namespace Centipede
     class Bullet : SpriteGameObject
     {
         Vector2 StartPosition;
+        Vector2 FirePosition;
+        private int speed = 200;
 
         public Bullet() : base("spr_bullet")
         {
@@ -23,6 +26,22 @@ namespace Centipede
 
             this.Velocity = Vector2.Zero;
             this.Position = StartPosition;
+        }
+
+        public void Fire()
+        {
+            this.Position = FirePosition;
+            this.Velocity = new Vector2(0, -speed);
+        }
+
+        public override void HandleInput(InputHelper inputHelper)
+        {
+            base.HandleInput(inputHelper);
+
+            if (inputHelper.IsKeyDown(Keys.Space))
+            {
+                Fire();
+            }
         }
     }
 }
