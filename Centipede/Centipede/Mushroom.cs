c2c80bb55b4f440e3e15adad56e531e8b47dc802
DarthWolf13 <martijn.vander.meer@quicknet.nl>
2017-03-21 10:31:08 +0100

    Step 5: Msuhroom class

---
+++ Centipede/Centipede/Mushroom.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Centipede
+{
+    class Mushroom : SpriteGameObject
+    {
+        int randomX = GameEnvironment.Random.Next(0, 470);
+        int randomY = GameEnvironment.Random.Next(25, 450);
+        public static int mushAmount = 20;
+
+        public Mushroom() : base("spr_mushroom")
+        {
+            this.Position = new Vector2(randomX, randomY);
+        }
+    }
+}
